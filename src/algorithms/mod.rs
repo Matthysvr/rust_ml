544d3bfc1f733f5de8b7e291a212ce1e49fc3d97
Matthysvr <>
2024-07-11 14:40:28 +0200

    Fix example tests

--- src/algorithms/mod.rs
+++ src/algorithms/mod.rs
@@ -1,9 +1,11 @@
+// src/algorithms/mod.rs
 pub trait Algorithm {
-    fn train(&self, data: &Vec<Vec<f64>>, labels: &Vec<f64>);
+    fn train(&mut self, data: &Vec<Vec<f64>>, labels: &Vec<f64>);
     fn predict(&self, data: &Vec<Vec<f64>>) -> Vec<f64>;
 }
 
 pub mod linear_regression;
-pub mod logistic_regression;
-pub mod decision_trees;
-pub mod neural_networks;
+pub use linear_regression::LinearRegression;
+// pub mod logistic_regression;
+// pub mod decision_trees;
+// pub mod neural_networks;
